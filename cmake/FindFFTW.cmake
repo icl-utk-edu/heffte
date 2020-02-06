660a3c757721b4b8a0ed4a5e8305d615f4dfd917
Miroslav Stoyanov <mkstoyanov@gmail.com>
2020-02-06 05:00:12 +0000

    Merged in mkstoyanov/heffte/simpler_find_fftw (pull request #4)
    simpler find fftw3
    
    Approved-by: Stan Tomov <tomov@eecs.utk.edu>

--- cmake/FindFFTW.cmake
+++ cmake/FindFFTW.cmake
@@ -8,6 +8,9 @@
 #   FFTW_LIBRARIES           ... full path to fftw library
 #   FFTW_INCLUDES            ... fftw include directory
 #
+# The file creates the imported target
+#   HEFFTE::FFTW             ... allows liking to the FFTW package
+#
 # The following variables will be checked by the function
 #   FFTW_USE_STATIC_LIBS    ... if true, only static libraries are found
 #   FFTW_ROOT               ... if set, the libraries are exclusively searched
@@ -15,6 +18,46 @@
 #   FFTW_LIBRARY            ... fftw library to use
 #   FFTW_INCLUDE_DIR        ... fftw include directory
 #
+
+macro(heffte_find_fftw_libraries)
+# Usage:
+#   heffte_find_fftw_libraries(PREFIX <fftw-root>
+#                              VAR <list-name>
+#                              REQUIRED <list-names, e.g., "fftw3" "fftw3f">
+#                              OPTIONAL <list-names, e.g., "fftw3_threads">)
+#  will append the result from find_library() to the <list-name>
+#  both REQUIRED and OPTIONAL libraries will be searched
+#  if PREFIX is true, then it will be searched exclusively
+#                     otherwise standard paths will be used in the search
+#  if a library listed in REQUIRED is not found, a FATAL_ERROR will be raised
+#
+  cmake_parse_arguments(heffte_fftw "" "PREFIX;VAR" "REQUIRED;OPTIONAL" ${ARGN})
+  foreach(_lib ${heffte_fftw_REQUIRED} ${heffte_fftw_OPTIONAL})
+    if (heffte_fftw_PREFIX)
+      find_library(
+          heffte_fftw_lib
+          NAMES "${_lib}"
+          PATHS ${heffte_fftw_PREFIX}
+          PATH_SUFFIXES "lib" "lib64"
+          NO_DEFAULT_PATH
+      )
+    else()
+      # not sure what LIB_INSTALL_DIR means
+      find_library(
+          heffte_fftw_lib
+          NAMES "${_lib}"
+          PATHS ${PKG_FFTW_LIBRARY_DIRS} ${LIB_INSTALL_DIR}
+      )
+    endif()
+    if (heffte_fftw_lib)
+      list(APPEND ${heffte_fftw_VAR} ${heffte_fftw_lib})
+      unset(heffte_fftw_lib CACHE)
+    elseif (${heffte_fftw_lib} IN_LIST ${heffte_fftw_REQUIRED})
+      message(FATAL_ERROR "Could not find required fftw3 library: ${heffte_fftw_lib}")
+    endif()
+  endforeach()
+  unset(_lib)
+endmacro(heffte_find_fftw_libraries)
 
 #If environment variable FFTW_ROOT_DIR is specified, it has same effect as FFTW_ROOT
 if( NOT FFTW_ROOT AND ENV{FFTW_ROOT_DIR} )
@@ -38,59 +81,14 @@ else()
   set( CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_SHARED_LIBRARY_SUFFIX} )
 endif()
 
+heffte_find_fftw_libraries(
+    PREFIX ${FFTW_ROOT}
+    VAR FFTW_LIBRARIES
+    REQUIRED "fftw3" "fftw3f"
+    OPTIONAL "fftw3_threads" "fftw3f_threads"
+  )
+
 if( FFTW_ROOT )
-
-  #find libs (double precision)
-  find_library(
-    FFTW_LIB
-    NAMES "fftw3"
-    PATHS ${FFTW_ROOT}
-    PATH_SUFFIXES "lib" "lib64"
-    NO_DEFAULT_PATH
-  )
-
-  find_library(
-    FFTW_THREADS_LIB
-    NAMES "fftw3_threads"
-    PATHS ${FFTW_ROOT}
-    PATH_SUFFIXES "lib" "lib64"
-    NO_DEFAULT_PATH
-  )
-
-  #find libs (single precision)
-  find_library(
-    FFTWF_LIB
-    NAMES "fftw3f"
-    PATHS ${FFTW_ROOT}
-    PATH_SUFFIXES "lib" "lib64"
-    NO_DEFAULT_PATH
-  )
-
-  find_library(
-    FFTWF_THREADS_LIB
-    NAMES "fftw3f_threads"
-    PATHS ${FFTW_ROOT}
-    PATH_SUFFIXES "lib" "lib64"
-    NO_DEFAULT_PATH
-  )
-
-#  #find libs (long double precision)
-#  find_library(
-#    FFTWL_LIB
-#    NAMES "fftw3l"
-#    PATHS ${FFTW_ROOT}
-#    PATH_SUFFIXES "lib" "lib64"
-#    NO_DEFAULT_PATH
-#  )
-#
-#  find_library(
-#    FFTWL_THREADS_LIB
-#    NAMES "fftw3l_threads"
-#    PATHS ${FFTW_ROOT}
-#    PATH_SUFFIXES "lib" "lib64"
-#    NO_DEFAULT_PATH
-#  )
-
   #find includes
   find_path(
     FFTW_INCLUDES
@@ -101,44 +99,6 @@ if( FFTW_ROOT )
   )
 
 else()
-
-  #find libs (double precision)
-  find_library(
-    FFTW_LIB
-    NAMES "fftw3"
-    PATHS ${PKG_FFTW_LIBRARY_DIRS} ${LIB_INSTALL_DIR}
-  )
-  find_library(
-    FFTW_THREADS_LIB
-    NAMES "fftw3_threads"
-    PATHS ${PKG_FFTW_LIBRARY_DIRS} ${LIB_INSTALL_DIR}
-  )
-
-  #find libs (single precision)
-  find_library(
-    FFTWF_LIB
-    NAMES "fftw3f"
-    PATHS ${PKG_FFTW_LIBRARY_DIRS} ${LIB_INSTALL_DIR}
-  )
-  find_library(
-    FFTWF_THREADS_LIB
-    NAMES "fftw3f_threads"
-    PATHS ${PKG_FFTW_LIBRARY_DIRS} ${LIB_INSTALL_DIR}
-  )
-#
-#
-#  #find libs (long double precision)
-#  find_library(
-#    FFTWL_LIB
-#    NAMES "fftw3l"
-#    PATHS ${PKG_FFTW_LIBRARY_DIRS} ${LIB_INSTALL_DIR}
-#  )
-#  find_library(
-#    FFTWL_THREADS_LIB
-#    NAMES "fftw3l_threads"
-#    PATHS ${PKG_FFTW_LIBRARY_DIRS} ${LIB_INSTALL_DIR}
-#  )
-
   find_path(
     FFTW_INCLUDES
     NAMES "fftw3.h"
@@ -147,28 +107,14 @@ else()
 
 endif( FFTW_ROOT )
 
-set(FFTW_LIBRARIES ${FFTW_LIB} ${FFTWF_LIB})
-
-# add threaded fftw lib if found
-if(FFTW_THREADS_LIB)
-  set(FFTW_LIBRARIES ${FFTW_LIBRARIES} ${FFTW_THREADS_LIB})
-endif()
-if(FFTWF_THREADS_LIB)
-  set(FFTW_LIBRARIES ${FFTW_LIBRARIES} ${FFTWF_THREADS_LIB})
-endif()
-
-# add long double fftw if found
-if(FFTWL_LIB)
-  set(FFTW_LIBRARIES ${FFTW_LIBRARIES} ${FFTWL_LIB})
-endif()
-if(FFTWL_THREADS_LIB)
-  set(FFTW_LIBRARIES ${FFTW_LIBRARIES} ${FFTWL_THREADS_LIB})
-endif()
-
 set( CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES_SAV} )
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(FFTW DEFAULT_MSG
                                   FFTW_INCLUDES FFTW_LIBRARIES)
 
-mark_as_advanced(FFTW_INCLUDES FFTW_LIBRARIES FFTW_LIB FFTWF_LIB FFTWL_LIB)
+add_library(HEFFTE::FFTW INTERFACE IMPORTED GLOBAL)
+target_link_libraries(HEFFTE::FFTW INTERFACE ${FFTW_LIBRARIES})
+set_target_properties(HEFFTE::FFTW PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${FFTW_INCLUDES})
+
+unset(CMAKE_FIND_LIBRARY_SUFFIXES_SAV)
