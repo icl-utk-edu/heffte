b17be4bf8c0aa7f0b9c8f424ad7594b51f9e90a8
Miroslav Stoyanov <stoyanovmk@ornl.gov>
2020-09-21 13:13:48 -0400

    added fortran swappers though swig

--- include/heffte_fft3d.h
+++ include/heffte_fft3d.h
@@ -199,6 +199,31 @@ public:
         static_assert(backend::is_enabled<backend_tag>::value, "The requested backend is invalid or has not been enabled.");
     }
 
+    //! \brief Internal use only, used by the Fortran interface
+    fft3d(int il0, int il1, int il2, int ih0, int ih1, int ih2, int io0, int io1, int io2,
+          int ol0, int ol1, int ol2, int oh0, int oh1, int oh2, int oo0, int oo1, int oo2,
+          MPI_Comm const comm,
+          bool use_reorder, bool use_alltoall, bool use_pencils)
+        : fft3d(box3d({il0, il1, il2}, {ih0, ih1, ih2}, {io0, io1, io2}),
+                box3d({ol0, ol1, ol2}, {oh0, oh1, oh2}, {oo0, oo1, oo2}),
+                comm,
+                plan_options(use_reorder, use_alltoall, use_pencils))
+    {}
+    //! \brief Internal use only, used by the Fortran interface
+    fft3d(int il0, int il1, int il2, int ih0, int ih1, int ih2, int io0, int io1, int io2,
+          int ol0, int ol1, int ol2, int oh0, int oh1, int oh2, int oo0, int oo1, int oo2,
+          MPI_Comm const comm)
+        : fft3d(box3d({il0, il1, il2}, {ih0, ih1, ih2}, {io0, io1, io2}),
+                box3d({ol0, ol1, ol2}, {oh0, oh1, oh2}, {oo0, oo1, oo2}),
+                comm)
+    {}
+    //! \brief Internal use only, used by the Fortran interface
+    fft3d(int il0, int il1, int il2, int ih0, int ih1, int ih2,
+          int ol0, int ol1, int ol2, int oh0, int oh1, int oh2,
+          MPI_Comm const comm)
+        : fft3d(box3d({il0, il1, il2}, {ih0, ih1, ih2}), box3d({ol0, ol1, ol2}, {oh0, oh1, oh2}), comm)
+    {}
+
     //! \brief Returns the size of the inbox defined in the constructor.
     int size_inbox() const{ return pinbox->count(); }
     //! \brief Returns the size of the outbox defined in the constructor.
