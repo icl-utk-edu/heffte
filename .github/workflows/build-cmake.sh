52310a494374046f325666101cd5815840cb370d
G-Ragghianti <ragghianti@icl.utk.edu>
2023-03-06 13:52:41 -0500

    Suggested edits

--- .github/workflows/build-cmake.sh
+++ .github/workflows/build-cmake.sh
@@ -7,28 +7,30 @@ source $(dirname $0)/init.sh
 
 ARGS="-DCMAKE_INSTALL_PREFIX=install"
 if [ "$BACKEND" = "MKL" ]; then
+   ARGS+="-DHeffte_ENABLE_MKL=ON"
    module try-load intel-mkl
    [ -z "$MKLROOT" ] && echo "Error loading MKL!" && exit 1
 elif [ "$BACKEND" = "FFTW" ]; then
+   ARGS+="-DHeffte_ENABLE_FFTW=ON"
    module try-load fftw
    fftw-wisdom
-elif [[ "$BACKEND" == "ONEAPI" || "$BACKEND" == "gpu_intel" ]]; then
+elif [[ "$BACKEND" == "ONEAPI" || "$ENABLE" == "gpu_intel" ]]; then
    module try-load intel-oneapi-mkl
    module try-load intel-oneapi-compilers
-   BACKEND="ONEAPI"
+   ARGS+=" -DHeffte_ENABLE_AVX=ON"
    ARGS+=" -D CMAKE_CXX_COMPILER=icpx -D Heffte_ONEMKL_ROOT=$MKLROOT"
    [ -z "$MKLROOT" ] && echo "Error loading OneAPI-MKL!" && exit 1
 elif [ "$BACKEND" = "gpu_nvidia" ]; then
-   BACKEND="CUDA"
+   ARGS+=" -DHeffte_ENABLE_CUDA=ON"
    module try-load cuda
    which nvcc
 elif [ "$BACKEND" = "gpu_amd" ]; then
-   BACKEND="ROCM"
+   ARGS+=" -DHeffte_ENABLE_ROCM=ON"
    export PATH=/opt/rocm/bin:$PATH
    which hipcc
 else
    # Use the stock backend with AVX instruction set
-   BACKEND=AVX
+   ARGS+=" -DHeffte_ENABLE_AVX=ON"
 fi
 
 [ "$STAGE" = "build" ] && rm -rf build install || true
@@ -36,13 +38,13 @@ mkdir -p build
 cd build
 
 if [ "$STAGE" = "build" ]; then
-   cmake $ARGS -DHeffte_ENABLE_$BACKEND=ON ..
+   cmake $ARGS ..
    make -j4
    make install
    ls -l install/lib*/libheffte.so
 elif [ "$STAGE" = "test" ]; then
-   make test
+   make ctest -V
 elif [ "$STAGE" = "smoketest" ]; then
-   echo Smoke test not implemented
+   make test_install
 fi
 
