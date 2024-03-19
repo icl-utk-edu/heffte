2b56df7759f9a4fa64944e7b121be9b0e328d4c6
G-Ragghianti <ragghianti@icl.utk.edu>
2024-03-19 09:36:11 -0400

    Disabling rocm ci runs

--- .github/workflows/build-spack.sh
+++ .github/workflows/build-spack.sh
@@ -16,12 +16,12 @@ source ../spack/share/spack/setup-env.sh
 VARIANTS=""
 if [ "$BACKEND" = "FFTW" ]; then
    VARIANTS="+fftw"
-elif [ "$BACKEND" = "MKL" ]; then
-   VARIANTS="+mkl"
+#elif [ "$BACKEND" = "MKL" ]; then
+#   VARIANTS="+mkl"
 elif [ "$BACKEND" = "gpu_nvidia" ]; then
-   VARIANTS="+cuda cuda_arch=70 ^cuda@11.4.3"
-elif [ "$BACKEND" = "gpu_amd" ]; then
-   VARIANTS="+rocm amdgpu_target=gfx90a ^hip@5.1.3"
+   VARIANTS="+cuda cuda_arch=70 ^cuda@11.8.0"
+#elif [ "$BACKEND" = "gpu_amd" ]; then
+#   VARIANTS="+rocm amdgpu_target=gfx90a ^hip@5.1.3"
 fi
 
 SPEC="heffte@develop $VARIANTS ^$MPI %$COMPILER"
