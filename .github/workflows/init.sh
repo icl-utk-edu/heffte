16bda8388091b20e834795edd2dbd78357b9e1f9
G-Ragghianti <ragghianti@icl.utk.edu>
2024-08-27 16:02:04 -0400

    updating spack location

--- .github/workflows/init.sh
+++ .github/workflows/init.sh
@@ -4,5 +4,6 @@ set -e
 trap 'echo "# $BASH_COMMAND"' DEBUG
 #shopt -s expand_aliases
 
-source /apps/spacks/current/github_env/heffte/share/spack/setup-env.sh
+source /apps/spacks/current/github_env/share/spack/setup-env.sh
+spack env activate heffte
 
