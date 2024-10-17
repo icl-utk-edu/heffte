5fb78ef924ea1db51c758c0c9dfe1b083e21546c
Christoph Junghans <junghans@votca.org>
2024-10-17 11:11:23 -0600

    cmake: fix two more paths

--- test/post_install_test.cmake.sh
+++ test/post_install_test.cmake.sh
@@ -19,14 +19,14 @@ fi
 @CMAKE_COMMAND@ \
     -DCMAKE_CXX_COMPILER=@CMAKE_CXX_COMPILER@ \
     -DCMAKE_CXX_FLAGS="@CMAKE_CXX_FLAGS@" \
-    -DHeffte_DIR=@CMAKE_INSTALL_PREFIX@/lib/cmake/Heffte \
+    -DHeffte_DIR=@CMAKE_INSTALL_FULL_LIBDIR@/cmake/Heffte \
     -DMPI_CXX_COMPILER="@MPI_CXX_COMPILER@" \
     $heffte_mpic_compiler \
     $heffte_mpif_compiler \
     -DMPIEXEC_NUMPROC_FLAG="@MPIEXEC_NUMPROC_FLAG@" \
     -DMPIEXEC_PREFLAGS="@MPIEXEC_PREFLAGS@" \
     -DMPIEXEC_POSTFLAGS="@MPIEXEC_POSTFLAGS@" \
-    @CMAKE_INSTALL_PREFIX@/share/heffte/testing/
+    @CMAKE_INSTALL_FULL_DATADIR@/heffte/testing/
 
 make -j4
 ctest -V
